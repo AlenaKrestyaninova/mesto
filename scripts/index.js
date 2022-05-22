0e38be648a1bdc0d22bd0cc3c5e6d5ae867874e5
Alenochka <alen4k@gmail.com>
2022-05-22 11:31:57 +0300

    project complete

--- scripts/index.js
+++ scripts/index.js
@@ -8,13 +8,27 @@ let jobInput = document.querySelector('.popup__input_occupation');
 let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle')
 
+function openPopup(popupElement) {
+    popupElement.classList.add('popup_opened');
+};
+
+function closePopup(popupElement) {
+    popupElement.classList.remove('popup_opened');
+}
+
 editButton.addEventListener('click', function(){
-    popup.classList.add('popup_opened');
+    openPopup(popup);
     nameInput.value = profileTitle.textContent;
     jobInput.value = profileSubtitle.textContent;
 });
 
-closePopupButton.addEventListener('click', function(){
-    popup.classList.remove('popup_opened');
+closePopupButton.addEventListener('click', function() {
+    closePopup(popup)
 });
 
+formElement.addEventListener('submit', function(event){
+    event.preventDefault();
+    profileTitle.textContent = nameInput.value;
+    profileSubtitle.textContent = jobInput.value;
+    closePopup(popup);
+});
